f2fa174cbd151dd09618d3cd24f67e912b597065
vedantSase <sasevedant5264@gmail.com>
2025-02-13 17:03:03 +0530

    Main server file

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,11 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization) {
+        next();
+    } else {
+        return res.status(401).json({ message: "Unauthorized. Please log in." });
+    }
 });
  
 const PORT =5000;
